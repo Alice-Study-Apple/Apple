65ef4abd138181b0c5163383ae64dc74891e5db0
bm4656 <newjin46@gmail.com>
2022-10-13 14:43:04 +0900

    feat: main2, main3 슬라이드만 구현 (트랜지션 추가해야함)

--- Store/index.js
+++ Store/index.js
@@ -1,3 +1,4 @@
+// main1 slider
 let slider = document.querySelector(".slider");
 let innerSlider = document.querySelector(".slider-inner");
 let pressed = false;
@@ -40,5 +41,50 @@ function checkboundary() {
     innerSlider.style.left = "0px";
   } else if (inner.right < outer.right) {
     innerSlider.style.left = `-${inner.width - outer.width}px`;
+  }
+}
+// main2 swiper
+let swiper = document.querySelector(".swiper");
+let innerSwiper = document.querySelector(".swiper-inner");
+let pressed_s = false;
+let on_s = false;
+let startx_s;
+let x_s;
+
+swiper.addEventListener("mouseover", (e) => {
+  pressed_s = true;
+  startx_s = e.offsetX - innerSwiper.offsetLeft;
+  swiper.style.cursor = "pointer";
+});
+
+swiper.addEventListener("mouseenter", () => {
+  swiper.style.cursor = "pointer";
+});
+
+swiper.addEventListener("mouseout", () => {
+  swiper.style.cursor = "pointer";
+});
+
+window.addEventListener("mouseout", () => {
+  pressed_s = false;
+});
+
+swiper.addEventListener("mousemove", (e) => {
+  if (!pressed_s) return;
+  e.preventDefault();
+  x_s = e.offsetX;
+
+  innerSwiper.style.left = `${x_s - startx_s}px`;
+  checkboundary_s();
+});
+
+function checkboundary_s() {
+  let outer = swiper.getBoundingClientRect();
+  let inner = innerSwiper.getBoundingClientRect();
+
+  if (parseInt(innerSwiper.style.left) > 0) {
+    innerSwiper.style.left = "0px";
+  } else if (inner.right < outer.right) {
+    innerSwiper.style.left = `-${inner.width - outer.width}px`;
   }
 }
